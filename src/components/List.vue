614fce0f49498830746775c31c59bb2627710bfb
Rafael Torrealba <rafaeltorrealba6@gmail.com>
2021-06-24 12:49:30 -0400

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container text-center">
+    <h1>Mi lista</h1>
+
+    <ol class="list-group list-group-numbered">
+      <li
+        v-for="(user, index) of arrayOrdenado"
+        :key="user.id"
+        @click="adding(index)"
+        class="list-group-item d-flex justify-content-between align-items-start"
+      >
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">{{ user.name }}</div>
+        </div>
+        <span class="badge bg-primary rounded-pill">{{ user.cantidad }}</span>
+      </li>
+    </ol>
+    <button class="btn btn-info btn-block" @click="restoring()">Restaurar</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "List",
+  computed: {
+    ...mapState(["users"]),
+    arrayOrdenado(){
+        return this.users.sort((a, b) => b.cantidad - a.cantidad)
+    }
+  },
+  methods: {
+    ...mapMutations(["adding", "restoring"]),
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: [
+      {
+        name: 'Rafael',
+        cantidad: 0,
+      },
+      {
+        name: 'Daniel',
+        cantidad: 0,
+      },
+      {
+        name: 'Moisés',
+        cantidad: 0,
+      },
+    ],
   },
   mutations: {
+    adding(state, index) {
+      state.users[index].cantidad++
+      this.ordering
+    },
+    restoring(state) {
+      state.users.forEach((user) => {
+        user.cantidad = 0
+      })
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {},
 })
